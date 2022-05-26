627de648ad2c6f9c71e9ad9faf8caff9e550bfee
MagicManik <manikislam1952@gmail.com>
2022-05-26 22:11:08 +0600

    heroku

--- index.js
+++ index.js
@@ -157,6 +157,15 @@ async function run() {
         });
 
 
+
+        app.delete('/tool/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await toolCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
     }
 
     finally {
